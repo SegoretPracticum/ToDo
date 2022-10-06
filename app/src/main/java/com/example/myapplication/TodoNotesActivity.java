f2887e0a18cf0c86147c66a84a303bbd2e1e5239
Segoret <eugsegoret@gmail.com>
2022-10-06 23:45:27 +0300

    fix problems branch recyclerview

--- app/src/main/java/com/example/myapplication/TodoNotesActivity.java
+++ app/src/main/java/com/example/myapplication/TodoNotesActivity.java
@@ -1,6 +1,4 @@
 package com.example.myapplication;
-
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
@@ -9,38 +7,34 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 
-
 public class TodoNotesActivity extends AppCompatActivity {
-    public RecyclerView notesView;
+    private final int ind = 1;
     public static final String NEW_NOTE = "NEW_NOTE";
-    private final ArrayList<TodoNotes> todoNotes= new ArrayList<>();
+    private final ArrayList<TodoNotes> todoNotes = new ArrayList<>();
 
-    @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        notesView = findViewById(R.id.resView);
+        RecyclerView notesView = findViewById(R.id.resView);
         Button addNote = findViewById(R.id.addNote);
         NotesAdapter notesAdapter = new NotesAdapter(todoNotes);
         notesView.setLayoutManager(new LinearLayoutManager(this));
         notesView.setAdapter(notesAdapter);
 
-         ActivityResultLauncher<Intent> noteResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-            Intent intent = result.getData();
-            TodoNotes newTodo = intent.getParcelableExtra(NEW_NOTE);
-            todoNotes.add(newTodo);
-            notesAdapter.notifyDataSetChanged();
+        ActivityResultLauncher<Intent> noteResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+            if (result.getData() != null) {
+                TodoNotes newTodo = result.getData().getParcelableExtra(NEW_NOTE);
+                todoNotes.add(newTodo);
+                notesAdapter.notifyItemChanged(ind);
+            }
         });
 
-                addNote.setOnClickListener(view -> {
-                    Intent addNewNote = new Intent(TodoNotesActivity.this, NoteEditActivity.class);
-                    noteResult.launch(addNewNote);
-                });
+        addNote.setOnClickListener(view -> {
+            Intent addNewNote = new Intent(TodoNotesActivity.this, NoteEditActivity.class);
+            noteResult.launch(addNewNote);
+        });
     }
-
-
-    }
+}
