0f4313a474bd518329f353b2462e8bdbe828839b
Segoret <eugsegoret@gmail.com>
2022-12-06 22:22:46 +0300

    fix the button addTodo view, fix EditText in TodoNotesActivity, canceling Actionbar, slightly changing activity_main.xml and activity_second.xml

--- app/src/main/java/com/example/myapplication/NoteEditActivity.java
+++ app/src/main/java/com/example/myapplication/NoteEditActivity.java
@@ -5,57 +5,53 @@ import static com.example.myapplication.TodoNotesActivity.TODO_NOTE;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import java.util.UUID;
 
 public class NoteEditActivity extends AppCompatActivity {
-    private EditText enterNote;
-    private TodoNotes finalNote;
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.menu_toolbar_button_send_note) {
-            Intent intent = new Intent();
-            if (enterNote.getText().toString().length() == 0) {
-                Toast.makeText(getApplicationContext(), R.string.note_edit_activity_edit_text_toast, Toast.LENGTH_SHORT).show();
-            } else {
-                finalNote.setNoteText(enterNote.getText().toString());
-                intent.putExtra(TODO_NOTE, finalNote);
-                setResult(RESULT_OK, intent);
-                finish();
-            }
-        }
-        return true;
-    }
+    private TodoNotes note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Toolbar toolbar = findViewById(R.id.note_edit_activity_toolbar);
-        setSupportActionBar(toolbar);
         toolbar.setNavigationOnClickListener(item -> finish());
-        enterNote = findViewById(R.id.note_edit_activity_edit_text_enter_note);
-        TodoNotes note = getIntent().getParcelableExtra(TODO_NOTE);
-        if (note != null) {
-            enterNote.setText(note.getNoteText());
+        EditText enterNote = findViewById(R.id.note_edit_activity_edit_text_enter_note);
+        note = getIntent().getParcelableExtra(TODO_NOTE);
+        processingIntent(enterNote, toolbar);
+        TodoNotes finalNote = note;
+        toolbar.setOnMenuItemClickListener(onMenuItemClickListener -> {
+            sendTodoNote(finalNote, enterNote);
+            return true;
+        });
+    }
+
+    private void sendTodoNote(TodoNotes todoNotes, EditText editText) {
+        Intent intent = new Intent();
+        if (editText.getText().toString().length() == 0) {
+            Toast.makeText(getApplicationContext(), R.string.note_edit_activity_edit_text_toast, Toast.LENGTH_SHORT).show();
         } else {
+            todoNotes.setNoteText(editText.getText().toString());
+            intent.putExtra(TODO_NOTE, todoNotes);
+            setResult(RESULT_OK, intent);
+            finish();
+        }
+    }
+
+    private void processingIntent(EditText editText, Toolbar toolbar) {
+        if (note != null) {
+            toolbar.setTitle("Edit todo");
+            editText.setText(note.getNoteText());
+        } else {
+            toolbar.setTitle("Add todo");
             note = new TodoNotes("", UUID.randomUUID().toString());
         }
-        finalNote = note;
     }
 }
+
