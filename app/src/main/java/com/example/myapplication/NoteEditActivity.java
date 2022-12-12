1ae6503c4aac6ad02fddb909bc4ccba76a84bc42
Segoret <eugsegoret@gmail.com>
2022-12-13 00:03:14 +0300

    add CardView with selector, change LayoutManager in RecyclerView from Linear to StaggeredGrid

--- app/src/main/java/com/example/myapplication/NoteEditActivity.java
+++ app/src/main/java/com/example/myapplication/NoteEditActivity.java
@@ -46,12 +46,11 @@ public class NoteEditActivity extends AppCompatActivity {
 
     private void processingIntent(EditText editText, Toolbar toolbar) {
         if (note != null) {
-            toolbar.setTitle("Edit todo");
+            toolbar.setTitle(R.string.note_edit_activity_toolbar_title_edit_todo);
             editText.setText(note.getNoteText());
         } else {
-            toolbar.setTitle("Add todo");
+            toolbar.setTitle(R.string.note_edit_activity_toolbar_title_add_todo);
             note = new TodoNotes("", UUID.randomUUID().toString());
         }
     }
 }
-
