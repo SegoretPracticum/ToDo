f5da16dec4bc101a5240d8c7d8705e1783e50ab3
Segoret <eugsegoret@gmail.com>
2022-12-22 14:53:22 +0300

    change distance in RecyclerView

--- app/src/main/java/com/example/myapplication/ViewHolder.java
+++ app/src/main/java/com/example/myapplication/ViewHolder.java
@@ -4,9 +4,8 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.android.material.card.MaterialCardView;
 
 public class ViewHolder extends RecyclerView.ViewHolder {
     private final TextView note;
@@ -14,8 +13,8 @@ public class ViewHolder extends RecyclerView.ViewHolder {
     public ViewHolder(@NonNull View itemView, NotesAdapter.OnTodoClickListener onTodoClickListener) {
         super(itemView);
         note = itemView.findViewById(R.id.item_notes_view_text_view);
-        MaterialCardView materialCardView = itemView.findViewById(R.id.card_todo);
-        materialCardView.setOnClickListener(view ->
+        CardView cardView = itemView.findViewById(R.id.card_todo);
+        cardView.setOnClickListener(view ->
                 onTodoClickListener.onTodoClick(getAdapterPosition()));
     }
 
